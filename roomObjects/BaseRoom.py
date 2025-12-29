81a7268e55b66221452e12c83d3a2a7c2754a3b5
iwhittaker18 <26488132@edgehill.ac.uk>
2025-12-29 01:42:19 +0000

    Changes the position atribute, add exit connections

--- roomObjects/BaseRoom.py
+++ roomObjects/BaseRoom.py
@@ -1,8 +1,18 @@
 #Begginning of the Base Room Class
 
 class BaseRoom:
-    def __init__(self, name, description, position):
+    def __init__(self, name, description):
         self.name = name #Name of the room e.g. Kitchen, Hall etc
         self.description = description #Description of the room, maybe like what it looks like
-        self.position = position # Positions of the room, north, east, south, west etc so the player can say things like go north
+        self.exits = {} #Ive decided to change the posiitons variable to exits, this is so that instead of storing where the room is on the map
+        #the program will now only store the positions of the exits so that the requirement in the brief that needs room connections is easier to code.
+        #It might still be possible to have a position attribute, to show the position of the room on the map as a whole rahter than just its connections with other rooms.
+        #Also by having the exits formatted like this it allows the program to not have hardcoded directions, makes it so that the user using the game engine
+        #can add the placements of the exits in the json file with a dictionary.
+
+
+    def addExit(self, direction, roomObject):
+        #This function would link the room that the player is current in, to another room in a specific direction.
+        self.exits[direction] = roomObject
+
         
